56b5828f83a4e1635dabcd54c0c3d141c959e907
EdwinLRT <75665728+EdwinLRT@users.noreply.github.com>
2023-04-28 18:36:53 +0200

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from controllers.menu import MenuController
-from views.menu import MenuViews
-def main():
-    MenuViews().display_menu_title()
-    MenuController().app_main_menu()
-
-if __name__ == '__main__':
-    main()
